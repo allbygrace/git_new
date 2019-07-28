fa8430b555a0b34fb05559f917ad93331ffdcc02
allbygrace <51425595+allbygrace@users.noreply.github.com>
2019-07-28 13:23:18 -0500

    MySQLDemo upload

---
+++ MySQLDemo/app/src/main/java/com/example/mysqldemo/DbStrings.java
@@ -0,0 +1,11 @@
+package com.example.mysqldemo;
+
+public class DbStrings {
+
+    static final String DATABASE_URL = "localhost:3306";
+    static final String DATABASE_NAME ="test";
+    static final String USERNAME ="root";
+    static final String PASSWORD = "l0rdipraiseu";
+
+
+}
